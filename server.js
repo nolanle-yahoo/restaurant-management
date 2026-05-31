de67a4f20bf0215e8ef573c39f647ba1c0989a56
nolanle-yahoo <nolanle@gmail.com>
2026-05-31 02:26:32 -0700

    Auto-commit: server.js

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ app.use('/api/reservations', require('./routes/reservations'));
 app.use('/api/menu',         require('./routes/menu'));
 app.use('/api/audit',        require('./routes/audit'));
 app.use('/api/payments',     require('./routes/payments'));
+app.use('/api/analytics',    require('./routes/analytics'));
 app.use('/api/public',       require('./routes/public'));
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
