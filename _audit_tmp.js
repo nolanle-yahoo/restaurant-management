0c34a41ab21e0d335154658e0c93168921e22500
nolanle-yahoo <nolanle@gmail.com>
2026-05-31 20:23:23 -0700

    Remove temporary audit script

--- _audit_tmp.js
+++
@@ -1,4 +0,0 @@
-const db = require('./db/database');
-const t = db.prepare("SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%' ORDER BY name").all().map(x => x.name);
-console.log('LIVE table count:', t.length);
-console.log(t.join(', '));
