c5a5e5687ac5ecf30291ba230b0802796e6c2e8a
nolanle-yahoo <nolanle@gmail.com>
2026-06-01 18:53:21 -0700

    Auto-commit: server.js

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.use('/api/marketing',    require('./routes/marketing'));
 app.use('/api/announcements',require('./routes/announcements'));
 app.use('/api/feedback',     require('./routes/feedback'));
 app.use('/api/waitlist',     require('./routes/waitlist'));
+app.use('/api/schedules',    require('./routes/schedules'));
 app.use('/api/public',       require('./routes/public'));
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
