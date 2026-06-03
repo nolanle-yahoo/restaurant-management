4a4c1b701e5e9329cd2932bc5cd7c47ce948f919
nolanle-yahoo <nolanle@gmail.com>
2026-06-03 00:47:42 -0700

    Auto-commit: routes/reservations.js

--- routes/reservations.js
+++ routes/reservations.js
@@ -3,6 +3,7 @@ const db = require('../db/database');
 const { verifyToken, requireRole } = require('../middleware/auth');
 const { broadcast } = require('../lib/ws');
 const { auditLog } = require('../lib/audit');
+const stripeLib = require('../lib/stripe');
 
 const router = express.Router();
 router.use(verifyToken);
