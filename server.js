c482a5e4e858ee8e299961cebf7ed29c3a95ac6c
nolanle-yahoo <nolanle@gmail.com>
2026-06-03 00:48:32 -0700

    Auto-commit: server.js

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ app.use('/api/schedules',    require('./routes/schedules'));
 app.use('/api/shift-swaps',  require('./routes/shift-swaps'));
 app.use('/api/regions',      require('./routes/regions'));
 app.use('/api/deliveries',   require('./routes/deliveries'));
+app.use('/api/customers',    require('./routes/customers'));
 app.use('/api/public',       require('./routes/public'));
 
 // Unknown routes fall back to the customer site (not the staff login).
